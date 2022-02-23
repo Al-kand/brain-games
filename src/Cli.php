945773b84e4f436b966d6cb0594e70078cf1192b
Aleks <my@mail.com>
2022-02-23 04:21:04 +0300

    fix lint errors

--- src/Cli.php
+++ src/Cli.php
@@ -1,10 +1,12 @@
 <?php
+
 namespace Brain\Games\Cli;
 
 use function cli\line;
 use function cli\prompt;
 
-function setUserName() {
+function setUserName()
+{
     line('Welcome to the Brain Game!');
     $name = prompt('May I have your name?');
     line("Hello, %s!", $name);
