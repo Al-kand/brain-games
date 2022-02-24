f69688eeb0707a6c24a9a6bc773835e3cfc76848
Aleks <my@mail.com>
2022-02-24 18:10:57 +0300

    fix welcome line

--- src/Engine.php
+++ src/Engine.php
@@ -9,8 +9,8 @@ const MAX_COUNTS = 3;
 
 function start(string $condition): void
 {
+    line('Welcome to the Brain Games!');
     global $name;
-    line('Welcome to the Brain Game!');
     $name = prompt('May I have your name?');
     line("Hello, %s!", $name);
     line($condition);
